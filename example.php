c097779fc4c54443601e80b779bdf79db58754d4
Timon Freitag <dev@timonf.de>
2013-09-30 21:44:33 +0200

    updated class using DOM parsing

--- example.php
+++ example.php
@@ -6,16 +6,18 @@ require 'vendor/autoload.php';
 use \Xbox\Gamertag;
 
 $gamertags = array(
-  'Major Nelson',
-  'Spartan 117'
+    'Major Nelson',
+    'Spartan 117',
+    'XoverBit',
+    'Invalid.#Gamertag'
 );
 
 echo "<table border=\"1\">";
-echo "<tr><th>Gamertag</th><th>Gamerscore</th><th>Name</th></th>";
+echo "<tr><th>Gamertag</th><th>Gamerscore</th><th>Is valid?</th></th>";
 foreach($gamertags as $gamertag) {
 
-  $gamertag = Gamertag::create($gamertag);
-  echo "<tr><td>{$gamertag->gamertag}</td><td>{$gamertag->score}</td><td>{$gamertag->name}</td></tr>";
+    $gamertag = Gamertag::create($gamertag);
+    echo "<tr><td>{$gamertag->gamertag}</td><td>{$gamertag->score}</td><td>".($gamertag->valid?'x':'--')."</td></tr>";
 
 }
 echo "</table>";
--- lib/Xbox/Gamertag.php
+++ lib/Xbox/Gamertag.php
@@ -2,123 +2,122 @@
 
 namespace Xbox;
 
-class Gamertag 
+class Gamertag
 {
-  
-  /** @var SimpleXMLElement */
-  protected $_xml = null;
-  
-  /** @var string contains the gamertag */
-  public $gamertag = null;
-  
-  /** @var string contains gamer's score */
-  public $score = null;
-  
-  /** @var string contains gamer's location */
-  public $location = null;
-  
-  /** @var string contains gamer's bio */
-  public $bio = null;
-  
-  /** @var string contains gamer's motto */
-  public $motto = null;
-  
-  /** @var string contains gamer's name */
-  public $name = null;
-  
-  /**
-   * @var bool is true when it is a valid gamertag
-   */
-  public $valid = false;
-  
-  /**
-   * converts HTML code into valid XML code
-   * @param string HTML code
-   * @return string XML code
-   */
-  private function htmlToXml($res)
-  {
-    // replace numeric entities
-    $res = preg_replace('~&#x([0-9a-f]+);~ei', 'chr(hexdec("\\1"))', $res);
-    $res = preg_replace('~&#([0-9]+);~e', 'chr("\\1")', $res);
-    
-    // replace literal entities
-    $trans_tbl = get_html_translation_table(HTML_ENTITIES);
-    $trans_tbl = array_flip($trans_tbl);
-    $res = strtr($res, $trans_tbl);
-    
-    // let's convert some utf8 signs into html entities
-    $res = str_replace('®','(R)',$res);
-    
-    // let's convert back all &-signs
-    $res = str_replace('&','&amp;',$res);
-    
-    /** @todo: cleanup non existing ascii codes */
-    
-    // SimpleXMLElement expects UTF-8 code
-    $res = utf8_decode($res);
-    
-    return $res;
-  }
-  
-  /**
-   * @example $gamertag = new Gamertag(@file_get_contents('http://gamercard.xbox.com/en-US/XoverBit.card'));
-   * @param string $res includes the whole gamertag
-   */
-  public function __construct($res)
-  {
-    if (is_string($res))
-    {
-      try {
-        $xml = $this->htmlToXml($res);
 
-        $this->_xml = new \SimpleXMLElement($xml);
-        
-        // read gamertag
-        $this->gamertag = (string)$this->_xml->head->title;
-        
-        // read gamer's score
-        $score = $this->_xml->xpath("//*[@id='Gamerscore']/text()");
-        $this->score = (string)current($score);
-        
-        // read gamer's location
-        $location = $this->_xml->xpath("//*[@id='Location']/text()");
-        $this->location = (string)current($location);
-        
-        // read gamer's bio
-        $bio = $this->_xml->xpath("//*[@id='Bio']/text()");
-        $this->bio = (string)current($bio);
-        
-        // read gamer's motto
-        $motto = $this->_xml->xpath("//*[@id='Motto']/text()");
-        $this->motto = (string)current($motto);
-        
-        // read gamer's name
-        $name = $this->_xml->xpath("//*[@id='Name']/text()");
-        $this->name = (string)current($name);
-        
-        // check if gamer is valid
-        $this->valid = (trim($this->score) !== '--');
-      } catch(\Exception $e) {
-        $this->valid = false;
-      }
-      
-    } else {
-      throw new \Exception("Resource must be a string.");
+    /** @var \DOMXPath */
+    protected $xpath = null;
+
+    /** @var string contains the gamertag */
+    public $gamertag = null;
+
+    /** @var int contains gamer's score */
+    public $score = null;
+
+    /**
+     * @var string contains gamer's location
+     * @deprecated information is not longer provided by Microsoft
+     */
+    public $location = null;
+
+    /**
+     * @var string contains gamer's bio
+     * @deprecated information is not longer provided by Microsoft
+     */
+    public $bio = null;
+
+    /**
+     * @var string contains gamer's motto
+     * @deprecated information is not longer provided by Microsoft
+     */
+    public $motto = null;
+
+    /**
+     * @var string contains gamer's name
+     * @deprecated information is not longer provided by Microsoft
+     */
+    public $name = null;
+
+    /**
+     * @var bool is true when it is a valid gamertag
+     */
+    public $valid = false;
+
+    /**
+     * @see http://stackoverflow.com/questions/1365756/how-to-view-domnodelist-objects-data-in-php
+     * @param \DOMNodeList $xpathResult
+     * @return string
+     */
+    protected function getDomText(\DOMNodeList $xpathResult)
+    {
+        $tempDom = new \DOMDocument();
+
+        foreach($xpathResult as $n) {
+            $tempDom->appendChild($tempDom->importNode($n,true));
+        }
+
+        return trim($tempDom->saveHTML());
     }
-  }
-  
-  /**
-   * build a Gamertag class using
-   * @param string gamertag
-   * @return Gamertag
-   */
-  static public function create($gamertag)
-  {
-    $c = get_called_class();
-    $gamertag = rawurlencode($gamertag);
-    $gamertagObject = new $c(@file_get_contents("http://gamercard.xbox.com/en-US/{$gamertag}.card"));
-    return $gamertagObject;
-  }
-  
+
+    /**
+     * @example $gamertag = new Gamertag(@file_get_contents('http://gamercard.xbox.com/en-US/XoverBit.card'));
+     * @param string $res includes the whole gamertag
+     */
+    public function __construct($res)
+    {
+        if (is_string($res))
+        {
+            try {
+                $dom = new \DOMDocument();
+                $dom->loadHTML($res);
+
+                $this->xpath = new \DOMXPath($dom);
+
+                // read gamertag
+                $this->gamertag = $this->getDomText($this->xpath->evaluate("/html/head/title/text()"));
+
+                // read gamer's score
+                $score = $this->getDomText($this->xpath->evaluate("//*[@id='Gamerscore']/text()"));
+                if ($score == '--') {
+                    throw new \Exception('Invalid gamertag');
+                } else {
+                    $this->score = (int)$score;
+                }
+
+                // read gamer's location
+                $this->location = $this->getDomText($this->xpath->evaluate("//*[@id='Location']/text()"));
+
+                // read gamer's bio
+                $this->bio = $this->getDomText($this->xpath->evaluate("//*[@id='Bio']/text()"));
+
+                // read gamer's motto
+                $this->motto = $this->getDomText($this->xpath->evaluate("//*[@id='Motto']/text()"));
+
+                // read gamer's name
+                $this->name = $this->getDomText($this->xpath->evaluate("//*[@id='Name']/text()"));
+
+                // check if gamer is valid
+                $this->valid = (trim($this->score) !== '--');
+            } catch(\Exception $e) {
+                $this->valid = false;
+            }
+
+        } else {
+            throw new \Exception("Resource must be a string.");
+        }
+    }
+
+    /**
+     * build a Gamertag class using
+     * @param string gamertag
+     * @return Gamertag
+     */
+    static public function create($gamertag)
+    {
+        $c = get_called_class();
+        $gamertag = rawurlencode($gamertag);
+        $gamertagObject = new $c(@file_get_contents("http://gamercard.xbox.com/en-US/{$gamertag}.card"));
+        return $gamertagObject;
+    }
+
 }
